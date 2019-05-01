3cdb0f609ea087021213f075f53f1e3e4f9e6a40
F1r3byte <f1r3byte@yandex.com>
2019-05-01 10:17:09 -1200

    edited config to suit project

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path');
 module.exports = {
-  entry: './src/Slider.jsx',
+  entry: './src/Glitchify.jsx',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
